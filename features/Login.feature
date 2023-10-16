0658ec9db5ed1e9409d6f1f0bf1d9df8ba538de0
unknown <raabrera@up.edu.ph>
2023-10-16 12:18:34 +0800

    add assertion

--- features/Login.feature
+++ features/Login.feature
@@ -1,15 +1,22 @@
-# Feature: Sample Feature
-
-#   Scenario: Opening Google
-#     Given I am on the Google homepage
-#     When I search for "Cucumber.js"
-#     Then I should see search results
-
-
 Feature: Login to SauceDemo
 
+  @positive 
   Scenario: Successful login
     Given I am on the SauceDemo login page
     When I enter the username "standard_user" and password "secret_sauce"
     And I click the login button
     Then I should be logged in
+
+  @negative
+  Scenario: Unsuccessful login
+    Given I am on the SauceDemo login page
+    When I enter the username "locked_out_user" and password "invalid_password"
+    And I click the login button
+    Then I should see an error message
+
+    @neutral
+    Scenario: Unsuccessful login
+        Given I am on the SauceDemo login page
+        When I enter the username "locked_out_user" and password "invalid_password"
+        And I click the login button
+        Then I should be logged in
--- features/stepdefinition/steps_def.js
+++ features/stepdefinition/steps_def.js
@@ -1,15 +1,25 @@
 const { Builder, By } = require('selenium-webdriver');
-const { Given, When, Then, After } = require('cucumber');
-var {setDefaultTimeout} = require('cucumber');
+const { Given, When, Then, After, setDefaultTimeout } = require('cucumber');
 setDefaultTimeout(60 * 1000);
+const firefox = require('selenium-webdriver/firefox');
+const reporter = require('cucumber-html-reporter');
+var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
 
 const URL = 'https://www.saucedemo.com/';
 
+chai.use(chaiAsPromised);
+var expect = chai.expect;
+
 Given('I am on the SauceDemo login page', async function () {
-  driver = new Builder()
-    .usingServer('http://localhost:4444/wd/hub') // Selenium server address
-    .forBrowser('firefox')
-    .build();
+    const options = new firefox.Options();
+    options.headless(); // Enable headless mode
+  
+    driver = new Builder()
+      .usingServer('http://localhost:4444/wd/hub') // Selenium server address
+      .forBrowser('firefox')
+      .setFirefoxOptions(options) // Set Firefox options with headless mode
+      .build();
 
   await driver.get(URL);
 });
@@ -29,11 +39,19 @@ When('I click the login button', async function () {
 });
 
 Then('I should be logged in', async function () {
+    const landingPage = await driver.getCurrentUrl();
+    expect(landingPage).to.equal("https://www.saucedemo.com/inventory.html");
+});
+
+Then('I should see an error message', async function () {
 
 });
 
-After(function () {
-  if (driver) {
-    driver.quit();
-  }
+After(async function () {
+    if (driver) {
+        
+        // Quit the WebDriver
+        await driver.quit();
+   
+      }
 });
